d3cd02eb0728a7eb464eb19cdbeb8959b2b97aa2
spadev <spadev2015@gmail.com>
2024-01-05 15:03:01 -0800

    start project
    -changed .html to .php
    -created data file

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <link rel="stylesheet" href="css/style.css">
+  <meta name="description" content="">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="icon" href="/favicon.ico" sizes="any">
+  <link rel="icon" href="/icon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="icon.png">
+
+  <link rel="manifest" href="site.webmanifest">
+  <meta name="theme-color" content="#fafafa">
+
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  
+</head>
+
+<body>
+
+<canvas id="myChart" width="400" height="400"></canvas>
+<script>
+        document.addEventListener('DOMContentLoaded', function() {
+            // Chart.js
+            var data = <?php echo file_get_contents("data/data.json"); ?>;
+            var ctx = document.getElementById('myChart').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'bar', // Change to 'line', 'pie', etc.
+                data: data,
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        });
+    </script>
+
+  <script src="js/app.js"></script>
+
+</body>
+
+</html>
